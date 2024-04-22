23d94b09f781975f70036cf992a6f95ea0616f53
Your Name <asta.carl@akqa.com>
2024-04-22 14:44:21 +0200

    Remove unused dependencies and deleted Spline component

--- src/components/StudioModels.jsx
+++ src/components/StudioModels.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import Studio from '../components/Spline';
 import Title from './ui/Title';
 
 
@@ -7,7 +6,6 @@ export default function StudioModels() {
   return (
     <div>
       <Title title="hejsa" variant="pageTitle"/>
-        <Studio/>
 
     </div>
     
