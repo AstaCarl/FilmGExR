cff4a9aa752c90a77424b75b1c03541fa756e38d
Your Name <asta.carl@akqa.com>
2024-05-16 14:23:13 +0200

    Remove Loader component and update styling in Home component

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -68,7 +68,6 @@ export default function Home({ homeData }) {
   } else {
     return (
       <main className={`transition-opacity ease-in duration-300 relative z-0 bg-off-white`}>
-        <Loader />
         <CookieBanner />
         <div>
           <HeroSection mobileSrc={mobileSrc} desktopSrc={desktopSrc} />
